dd025adc8b587b5ec37af2a984ada7888340e59d
eng-telur4 <eng.telur4@gmail.com>
2024-11-05 10:31:03 +0900

    modified ft_printf/test.c

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -5,10 +5,10 @@ int	main(void)
 	int	ret01;
 	int	ret02;
 
-	ret01 = ft_printf("12[%c]8[%%][%s]e%wr[%i]d[%p]f[%%][%d]32fd[%u]wfht43[%x][%X][%d]\n",
+	ret01 = ft_printf("12[%c]8[%%][%s]ewr[%i]d[%p]f[%%][%d]32fd[%u]wfht43[%x][%X][%d]\n",
 			'A', "BiSH", 123, (void *)"BiSH", INT_MAX, UINT_MAX, 0xFF, 0x6ff,
 			'A');
-	ret02 = printf("12[%c]8[%%][%s]e%wr[%i]d[%p]f[%%][%d]32fd[%u]wfht43[%x][%X][%d]\n",
+	ret02 = printf("12[%c]8[%%][%s]ewr[%i]d[%p]f[%%][%d]32fd[%u]wfht43[%x][%X][%d]\n",
 			'A', "BiSH", 123, (void *)"BiSH", INT_MAX, UINT_MAX, 0xFF, 0x6ff,
 			'A');
 	printf("length : %d\n", ret01);
